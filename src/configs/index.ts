74e3357a3b4d0278b833df178ea9c770ca3c9bcc
spacehaz <spacehaz@gmail.com>
2022-09-05 20:03:15 +0300

    added link sdk

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -11,7 +11,7 @@ const contracts: TContracts = {
     apiHost: 'https://mainnet-v1-1.linkdrop.io'
   }, // mainnet
   137: {
-    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53',
+    factory: '0x632E4Ef82188d466462Aecdc0193059C4Cd294Ec',
     claimHost: 'https://v1-2.claim.linkdrop.io',
     apiHost: 'https://matic-v1-1.linkdrop.io'
   }
