e097278036c6bf2bcc07e2a9b51782a3046ae291
spacehaz <spacehaz@gmail.com>
2022-09-05 18:28:22 +0300

    added link sdk

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -6,12 +6,12 @@ type TContracts = Record<string | number, {
 
 const contracts: TContracts = {
   1: {
-    factory: '0xadEA4080b6B3cA8CaB081ce839B3693DbBA8d480',
+    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53',
     claimHost: 'https://v1-2.claim.linkdrop.io',
     apiHost: 'https://mainnet-v1-1.linkdrop.io'
   }, // mainnet
   137: {
-    factory: '0xadEA4080b6B3cA8CaB081ce839B3693DbBA8d480',
+    factory: '0x926923238FE6f4866E7FB29a05538e7C4C118a53',
     claimHost: 'https://v1-2.claim.linkdrop.io',
     apiHost: 'https://matic-v1-1.linkdrop.io'
   }
