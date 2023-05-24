a41ddb7918264a0d89dc257fac7f2c63bc541d2a
spacehaz <spacehaz@gmail.com>
2023-05-24 18:55:58 +0200

    update for only_preferred_wallet

--- src/types/link-params/index.ts
+++ src/types/link-params/index.ts
@@ -18,7 +18,7 @@ type TLinkParams = {
   claiming_finished_description: string
   claiming_finished_button_title: string 
   claiming_finished_button_url: string
-  only_preferred_wallet: string
+  only_preferred_wallet: boolean
 }
 
 export default TLinkParams
